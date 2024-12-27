7bc649e1ba01d21184a33183de025407c4eaf9a2
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2024-12-27 12:06:43 +0600

    add User & Admin Dashboard |logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,8 +27,9 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(route('dashboard', absolute: false));
+        
+        return redirect(route('home.index'));
+        // return redirect()->intended(route('dashboard', absolute: false));
     }
 
     /**
