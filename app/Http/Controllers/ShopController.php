3509e8c7c25196edbd457eaa4f51f5e4107c4c7b
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-01-17 22:17:59 +0600

    Shop-page Products Sorting |Created_at & price wish

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,15 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Break_;
 
 class ShopController extends Controller
 {
     public function index(Request $request){
         $size=$request->query('size') ? $request->query('size') : 12;
-        $products=Product::orderBy('created_at','desc')->paginate($size);
+
+        $o_column= "";
+        $o_order= "";
+        $order=$request->query('order') ? $request->query('order') : -1;
+        switch($order){
+            case 1:
+                $o_column='created_at';
+                $o_order='DESC';
+                break;
+            case 2:
+                $o_column='created_at';
+                $o_order='ASC';
+                break;
+            case 3:
+                $o_column='sale_price';
+                $o_order='ASC';
+                break;
+            case 4:
+                $o_column='sale_price';
+                $o_order='DESC';
+                break;
+            default:
+                $o_column='id';
+                $o_order='DESC';
+        }
+        $products=Product::orderby($o_column,$o_order)->paginate($size);
         return view('shop',[
             'products'=>$products,
             'size'=>$size,
+            'order'=>$order,
         ]);
     }
 
