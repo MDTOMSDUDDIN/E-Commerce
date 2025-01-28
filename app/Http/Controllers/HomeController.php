bcd36f35e339682ec5f4da125ede89096f86fb87
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-01-28 11:09:56 +0600

    Sh On Sale Products On HomePage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\category;
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,11 @@ class HomeController extends Controller
     function index(){
         $slides=Slide::where('status',1)->get()->take(3);
         $categories=category::orderBy('name')->get();
+        $sproducts=Product::whereNotNull('sale_price')->where('sale_price','<>','')->inRandomOrder()->get()->take(8);
         return view('index',[
             'slides'=>$slides,
             'categories'=>$categories,
+            'sproducts'=>$sproducts,
         ]);
     }
 }
